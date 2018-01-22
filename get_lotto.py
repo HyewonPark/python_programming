bde1844fb1a29f22cca7cb10739611165721d788
renee <reneep@naver.com>
2018-01-22 07:34:44 +0000

    Get Lotto info from API

---
+++ get_lotto.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo="
+res = requests.get(url).json()
+drw_numbers = []
+
+for key, value in res.items():
+  if 'drwtNo' in key:
+    drw_numbers.append(value)
+	
+drw_numbers = sorted(drw_numbers)
+bonus_number = res['bnusNo']
+   
+print('이번주 로또 번호는: ', drw_numbers)
+print('보너스 번호는: ',bonus_number)
+
